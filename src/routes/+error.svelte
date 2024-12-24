0a15144990a505da5dea5c71037749ce9ea5d0d3
Joseph Kato <joseph@jdkato.io>
2024-12-24 15:40:29 -0700

    404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,6 @@
 <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
 	<div class="text-center">
-		<p class="text-base font-semibold">404</p>
+		<p class="text-base text-xl font-semibold text-lime-500">404</p>
 		<h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
 			Page not found
 		</h1>
@@ -10,7 +10,7 @@
 		<div class="mt-10 flex items-center justify-center gap-x-6">
 			<a
 				href="/"
-				class="rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+				class="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 				>Go back home</a
 			>
 			<a
