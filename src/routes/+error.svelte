529c3e7905ad7d41ef8b49afd20753c0c9196867
Joseph Kato <joseph@jdkato.io>
2025-01-07 22:37:48 -0800

    Better error handling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,7 @@
-<main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+<main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
 	<div class="text-center">
 		<p class="text-base text-xl font-semibold text-lime-500">404</p>
-		<h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-			Page not found
-		</h1>
+		<h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight">Page not found</h1>
 		<p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
 			Sorry, we couldn’t find the page you’re looking for.
 		</p>
@@ -13,9 +11,7 @@
 				class="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 				>Go back home</a
 			>
-			<a
-				href="https://github.com/errata-ai/vale.sh/issues/new"
-				class="text-sm font-semibold text-gray-900"
+			<a href="https://github.com/errata-ai/vale.sh/issues/new" class="text-sm font-semibold"
 				>Contact support <span aria-hidden="true">&rarr;</span></a
 			>
 		</div>
